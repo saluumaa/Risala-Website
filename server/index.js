bc153acc86f789d77d097322937736c7ec7858f1
saluumaa <salumahassan02@gmail.com>
2024-07-15 12:09:41 +0300

    Add nodemodules to ignore file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import userRoute from './routes/user.route.js';
