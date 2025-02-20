4fc2c87345c10275c3528f57a6219e32e96a7ed7
saluumaa <salumahassan02@gmail.com>
2025-02-20 08:14:15 +0300

    add baseurl server to the client

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,14 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import NewsReducer from './NewsSlice';
 import usersReducer from './UsersSlice';
-import chatReducer from './ChatSlice';
-import messageReducer from './MessageSlice';
+
 const store = configureStore({
     reducer: {
         news: NewsReducer,
         user: usersReducer,
-        chat: chatReducer,
-        message: messageReducer,
     }
 })
 
