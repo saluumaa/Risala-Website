c2f656d8a96c6e696e537106572b2054ea9e0b70
saluumaa <salumahassan02@gmail.com>
2025-12-14 21:41:25 +0300

    update womenempowerment import error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import {useSelector } from 'react-redux';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
