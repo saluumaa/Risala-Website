294123d34f2ae82acacd32d7e2ccc02ffa515d60
saluumaa <salumahassan02@gmail.com>
2025-12-14 21:34:37 +0300

    adjust the app folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,7 @@ import NewsManager from './Components/admin/NewsManager';
 
 // Services
 import Awareness from './Components/Services/awareness/Awareness';
-import WomenEmpowerment from './Components/Services/empowerment/WomenEmpowerment';
+import WomenEmpowerment from './Components/Services/empowerment/womenEmpowerment';
 import Education from './Components/Services/education/Education';
 
 
